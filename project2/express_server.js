c1e362a09963015f71ae431eea6ed02df8cc2e2d
Sara Han <marsa926@gmail.com>
2017-01-17 20:58:51 +0000

    added .ejs and links to ejs page

--- project2/express_server.js
+++ project2/express_server.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; //default
+//Express app need to use EJS as templating engine.
+app.set("view engine", "ejs");
+
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -19,10 +22,18 @@ var urlDatabase = {
 
 
 //this will print out HELLO WORLD(bold) in http://localhost:8080/hello
-app.get("/hello", function(request, response){
-  response.end("<html><body>HELLO <b>WORLD</b></body></html>\n");
+// app.get("/hello", function(request, response){
+//   response.end("<html><body>HELLO <b>WORLD</b></body></html>\n");
+// });
+
+app.get("/urls", function(request, response){
+  let templateVars = { urls: urlDatabase };
+  response.render("urls_index", templateVars);
+
+  // response.render("urls_index", { urls: urlDatabase });
 });
 
 app.listen(PORT, function(){
   console.log(`Example app listening on port ${PORT}!`);
 });
+
