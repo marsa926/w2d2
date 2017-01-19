30ee622a62a4e7f80cce555e8877bda595b141ea
Sara Han <marsa926@gmail.com>
2017-01-19 03:31:06 +0000

    forgot to add files today, adding all the new files, with edited codes, with some css as well

--- project2/express_server.js
+++ project2/express_server.js
@@ -1,27 +1,31 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; //default
-
+var bodyParser = require('body-parser');
 
 //Express app need to use EJS as templating engine.
 app.set("view engine", "ejs");
-
+app.use(bodyParser.urlencoded({extended:true}));
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-//generate random shortURL
-function generateRandomString(){
-  let randomStr = "";
-  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+ function generateRandomString(){
+    var shortURL = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     for (var i = 0; i < 6; i++){
-      randomStr += possible.charAt(Math.floor(Math.random() * possible.length));
+      let randNumb = Math.floor(Math.random() * possible.length);
+      shortURL += possible[randNumb];
     }
-    return randomStr;
-}
+    return shortURL;
+  }
 
+function updateDataBase(shortURL, newlongURL){
+  urlDatabase[shortURL] = newlongURL;
+  console.log(`updated ${shortURL} belongs with ${newlongURL}`);
+}
 
 //this will print out Hello! on http://localhost:8008
 // app.get("/", function(request, response){
@@ -39,30 +43,70 @@ function generateRandomString(){
 //   response.end("<html><body>HELLO <b>WORLD</b></body></html>\n");
 // });
 
-app.get("/urls", function(request, response){
+app.get("/urls", (request, response) =>{
   let templateVars = { urls: urlDatabase };
   response.render("urls_index", templateVars);
   // response.render("urls_index", { urls: urlDatabase });
 });
 
 //for urls_show, showing one shortURL and it's address
-app.get("/urls/:id", function(request, response){
+
+
+app.get("/urls/new", (request, response) =>{
+  response.render("urls_new");
+});
+
+// app.post("/urls", (request, response) =>{
+//   console.log(request.body); //debug statement to see POST para
+//   response.send(" OK ");
+
+// });
+
+
+
+
+app.get("/u/:shortURL", (request, response) =>{
+  let longURL = urlDatabase[request.params.shortURL];
+  response.redirect(longURL);
+});
+
+//generate random shortURL
+app.post("/urls/new", (request, response) =>{
+  var longURL = request.body.longURL;
+  var shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  // console.log(urlDatabase);
+  response.redirect(`/urls/${shortURL}`);
+});
+
+//delete the url
+app.post("/urls/:shortURL/delete", (request, response) =>{
+  let shortURL = request.params.shortURL;
+  delete urlDatabase[shortURL];
+  response.redirect("/urls");
+});
+
+//update the url
+app.post("/urls/:shortURL/update",(request, response) =>{
+  let shortURL = request.params.shortURL;
+  let newlongURL = request.body.newlongURL;
+  updateDataBase(shortURL, newlongURL);
+response.redirect("/urls/");
+});
+
+
+
+
+app.get("/urls/:id", (request, response) =>{
   let templateVars = { shortURL: request.params.id, urls: urlDatabase };
   response.render("urls_show", templateVars);
 });
 
-app.get("/urls/new", function(request, response){
-  response.render("urls_new");
-});
 
-app.post("urls/new", function(request, response){
-  console.log(request.body.form); //debug statement to see POST para
-  response.send("OK");
-});
 
 
 //listens to port if its working or not
-app.listen(PORT, function(){
+app.listen(PORT, () =>{
   console.log(`Example app listening on port ${PORT}!`);
 });
 
